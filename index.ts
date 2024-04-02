ee291008e8cd3d38646dff148534da9d445c60ed
haziqsiddiqui88 <siddiquihaziq12@gmail.com>
2024-04-03 04:17:28 +0500

    created  todos list

---
+++ index.ts
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+
+let todos = []
+let condition = true;
+
+while(condition)
+{
+  let todoQuestion= await inquirer.prompt(
+    [
+        {
+        name:"firstQuestion",
+        type:"input",
+        message:"what would you like to add in your todos?"
+    },
+    {
+        name:"secondQuestion",
+        type:"confirm",
+        messletage:"would you like to add more in your todos?",
+        default:"true"
+    }
+]
+);
+
+todos.push(todoQuestion.firstQuestion)
+console.log(todos);
+condition = todoQuestion.secondQuestion;
+}
